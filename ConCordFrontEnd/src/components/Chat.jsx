a22e58380fb4f47efc603853a4e06984683574f3
Yevgeniy Akimenko <akimenko.y@gmail.com>
2026-09-13 16:22:29 -0700

    style: remove dark mode box shadows and format component files

--- ConCordFrontEnd/src/components/Chat.jsx
+++ ConCordFrontEnd/src/components/Chat.jsx
@@ -15,7 +15,7 @@ export default function Chat({ username }) {
   const handleSelectChannel = (channel) => {
     setChannelSelected(channel)
   }
-  
+
   const handleSubmit = async (message) => {
     await fetch(`/api/channels/${channelSelected.id}/messages`, {
       method: 'POST',
@@ -87,12 +87,14 @@ export default function Chat({ username }) {
   }, [connection, channelSelected])
 
   useEffect(() => {
-    document.getElementById('chatDiv').scrollTo(0, document.getElementById('chatList').scrollHeight)
+    document
+      .getElementById('chatDiv')
+      .scrollTo(0, document.getElementById('chatList').scrollHeight)
   }, [messages])
 
   return (
     <div className="Chat h-full w-full flex flex-col sm:flex-row">
-      <Sidebar 
+      <Sidebar
         channels={channels}
         selectedChannel={channelSelected}
         onSelectChannel={handleSelectChannel}
@@ -100,19 +102,28 @@ export default function Chat({ username }) {
       />
 
       <div className="main h-3/4 grow sm:h-full w-full flex flex-col">
-        <div 
-          id='chatDiv' 
-          className="Messages bg-white dark:bg-black grow overflow-y-auto border rounded-md border-slate-500 shadow-md shadow-slate-400 dark:shadow-zinc-600 p-4 mb-4 sm:mt-4 sm:mx-4 "
+        <div
+          id="chatDiv"
+          className="Messages bg-white dark:bg-black grow overflow-y-auto border rounded-md border-slate-500 shadow-md shadow-slate-400 dark:shadow-none p-4 mb-4 sm:mt-4 sm:mx-4 "
         >
-          <ul id='chatList' className='text-left text-clip'>
-            {channelSelected && messages.map((message) => (
-              <Message key={message.id} message={message} username={username} />
-            ))}
+          <ul id="chatList" className="text-left text-clip">
+            {channelSelected &&
+              messages.map((message) => (
+                <Message
+                  key={message.id}
+                  message={message}
+                  username={username}
+                />
+              ))}
           </ul>
         </div>
-        
+
         {channelSelected && (
-          <Form userName={username} channelId={channelSelected.id} onSubmit={handleSubmit} />
+          <Form
+            userName={username}
+            channelId={channelSelected.id}
+            onSubmit={handleSubmit}
+          />
         )}
       </div>
     </div>
--- ConCordFrontEnd/src/components/Form.jsx
+++ ConCordFrontEnd/src/components/Form.jsx
@@ -11,27 +11,28 @@ export default function Form({ userName, channelId, onSubmit }) {
     onSubmit(message)
     text.value = ''
   }
-  
+
   return (
-    <div className='Form '>
-    <form onSubmit={handleSubmit}>
-      <div className='FormContents flex items-center sm:px-4'>
-        <input 
-          autoFocus 
-          maxLength={500}
-          placeholder={`Chatting as ${userName}`}
-          type="text" 
-          id="text" 
-          name="text" 
-          className='focus:outline-none grow dark:bg-black border rounded-md border-slate-500 shadow-md shadow-slate-400 dark:shadow-zinc-600 w-5/6 mr-2 px-4 py-2' 
-        />
-        <button 
-          type="submit" className='bg-orange-400 dark:bg-orange-600 hover:bg-orange-500 border rounded-md border-slate-500 shadow-md shadow-slate-400 dark:shadow-zinc-600 px-3 py-2'
-        >
-          📢
-        </button>
-      </div>
-    </form>
+    <div className="Form ">
+      <form onSubmit={handleSubmit}>
+        <div className="FormContents flex items-center sm:px-4">
+          <input
+            autoFocus
+            maxLength={500}
+            placeholder={`Chatting as ${userName}`}
+            type="text"
+            id="text"
+            name="text"
+            className="focus:outline-none grow dark:bg-black border rounded-md border-slate-500 shadow-md shadow-slate-400 dark:shadow-none w-5/6 mr-2 px-4 py-2"
+          />
+          <button
+            type="submit"
+            className="bg-orange-400 dark:bg-orange-600 hover:bg-orange-500 border rounded-md border-slate-500 shadow-md shadow-slate-400 dark:shadow-none px-3 py-2"
+          >
+            📢
+          </button>
+        </div>
+      </form>
     </div>
   )
 }
