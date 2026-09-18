4d2aa474a3c271cbcf8aa3e4c12afff498a78539
Yevgeniy Akimenko <akimenko.y@gmail.com>
2026-09-17 22:36:36 -0700

    refactor: improve message layout with flex and remove manual text wrapping

--- ConCordFrontEnd/src/components/Message.jsx
+++ ConCordFrontEnd/src/components/Message.jsx
@@ -12,20 +12,15 @@ export default function Message({ message, username }) {
       ? 'text-orange-600 dark:text-orange-500'
       : 'text-orange-800 dark:text-orange-700'
 
-  let wordArr = message.text.split(' ').map((word) => {
-    return word.length > 24 ? word.match(/.{1,24}/g) : word
-  })
-  let newWordArr = wordArr.flat()
-  const text = newWordArr.join(' ')
-
   return (
     <>
       <li className="mb-2">
-        <p className="">
-          <span className={`mr-2 ${myMessage}`}>
+        <p className="flex items-start">
+          <span className={`mr-2 block shrink-0 ${myMessage}`}>
             <span className="font-semibold">{message.userName}</span>:
           </span>
-          {text}
+
+          <span className="block">{message.text}</span>
         </p>
 
         <p className="text-xs text-slate-400">
